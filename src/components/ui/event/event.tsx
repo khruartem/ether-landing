6d91e41356d788a67e37b310a45170917885a170
Artem Khrustalev <khru-artem@yandex.ru>
2026-03-06 00:59:00 +0300

    fix:поправил ошибки в бейдже event

--- src/components/ui/event/event.tsx
+++ src/components/ui/event/event.tsx
@@ -1,4 +1,5 @@
 import type { FC } from "react";
+import clsx from "clsx";
 
 import { BadgeUI } from "../badge";
 import { BadgeTitleUI } from "../badge-title";
@@ -10,7 +11,7 @@ import type { TBadgeTitle } from "../../../utils/types";
 
 import styles from "./event.module.css";
 
-export const EventUI: FC<TEventUIProps> = ({name, date, time}) => {
+export const EventUI: FC<TEventUIProps> = ({ name, date, time }) => {
   const title: TBadgeTitle = {
     text: "Название мероприятия",
     decorated: true,
@@ -19,41 +20,25 @@ export const EventUI: FC<TEventUIProps> = ({name, date, time}) => {
   return (
     <BadgeUI className={styles["hero__block_event"]}>
       <div>
-      <BadgeTitleUI title={name} />
-      <InputUI placeholder="Новое мероприятие" />
+        <BadgeTitleUI title={name} />
+        <InputUI placeholder="Новое мероприятие" />
       </div>
-      <BadgeTitleUI
-        title={{
-          text: "Дата и время проведения",
-        }}
-      />
-      <div className={styles["date-time"]}>
-      <InputUI
-        withMask
-        id="date-picker"
-        name="date-picker"
-        type="text"
-        placeholder="Дата мероприятия"
-        mask="dd.mm.yyyy"
-        replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
-        showMask
-        className={clsx(
-          styles["date-time__input"],
-          styles["date-time__input_date"],
-        )}
-      />
-      <InputUI
-        withMask
-        id="date-picker"
-        name="date-picker"
-        type="text"
-        placeholder="Время мероприятия"
-        mask="hh:mm"
-        replacement={{ h: /\d/, m: /\d/ }}
-        showMask
-        className={styles["date-time__input"]}
-      />
-    </div>
+      <div>
+        <BadgeTitleUI title={date} />
+        <div className={styles["date-time"]}>
+          <InputUI
+            placeholder="Дата мероприятия"
+            className={clsx(
+              styles["date-time__input"],
+              styles["date-time__input_date"],
+            )}
+          />
+          <InputUI
+            placeholder="Время мероприятия"
+            className={styles["date-time__input"]}
+          />
+        </div>
+      </div>
       <EventType />
     </BadgeUI>
   );
