c0fca662d04200a6b322a35f767756bfef225169
Artem Khrustalev <khru-artem@yandex.ru>
2026-04-02 00:42:39 +0300

    feat:переделал иконки на компонент icon в лого ОТ

--- src/components/ui/back-to-main/back-to-main.tsx
+++ src/components/ui/back-to-main/back-to-main.tsx
@@ -1,19 +1,26 @@
 import type { FC } from "react";
+import { Link } from "react-router-dom";
+
+import { Icon } from "../../icon";
 
 import { OTLabel } from "../../ot-label";
-import { OTLogo } from "../../icons";
 
 import styles from "./back-to-main.module.css";
 
 export const BackToMainUI: FC = () => {
   return (
-    <a
-      href="https://otkter.ru/"
+    <Link
+      to="https://otkter.ru/"
       target="_blank"
       className={styles["back-to-main"]}
     >
       <OTLabel />
-      <OTLogo />
-    </a>
+      <Icon
+        name={"OT"}
+        width={34}
+        height={20}
+        className={styles["back-to-main__logo"]}
+      />
+    </Link>
   );
 };
