b147148612574ed0a47b147f82ae966da488d0c9
Artem Khrustalev <khru-artem@yandex.ru>
2026-04-14 00:10:34 +0300

    feat:переделал иконку arrow на компонент icon в секции faq

--- src/components/ui/question-open-button/question-open-button.tsx
+++ src/components/ui/question-open-button/question-open-button.tsx
@@ -1,9 +1,11 @@
 import type { FC } from "react";
 import clsx from "clsx";
 
+import { Icon } from "../../icon";
+
 import type { TQuestionOpenButtonUIProps } from "./types";
 
-import arrowSvg from "../../../assets/icons/chevron-down.svg";
+// import arrowSvg from "../../../assets/icons/chevron-down.svg";
 
 import styles from "./question-open-button.module.css";
 
@@ -17,9 +19,15 @@ export const QuestionOpenButtonUI: FC<TQuestionOpenButtonUIProps> = ({
         isOpened && styles.question__button_opened,
       )}
     >
-      <img
+      {/* <img
         src={arrowSvg}
         alt={"Иконка стрелочки"}
+        className={styles.question__arrow}
+      /> */}
+      <Icon
+        name={"arrow"}
+        width={24}
+        height={24}
         className={styles.question__arrow}
       />
     </button>
